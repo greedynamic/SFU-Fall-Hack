bf2679946368175ef37a5db6fbb614516eb8ca26
filipomarcellino <66431048+filipomarcellino@users.noreply.github.com>
2022-10-15 14:56:15 -0700

    added userid as prop in eventcard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ function Home(){
 			location: 'Rogers arena',
 			time: 'October 18th 2022',
 			cost: 0,
+			userID: 10
 		},
 		{
 			id: 2,
@@ -15,6 +16,8 @@ function Home(){
 			location: 'Toronto',
 			time: 'tomorrow',
 			cost: 0,
+			userID: 11
+
 		},
 		{
 			id: 3,
@@ -22,12 +25,14 @@ function Home(){
 			location: 'SFU Burnaby, AQ',
 			time: '7 pm',
 			cost: 0,
+			userID: 12
+
 		},
 	];
 	return (
 		<div>
 			{events.map((event)=>{
-				return <EventCard key={event.id} title={event.title} location={event.location} time={event.time} cost={event.cost} />;
+				return <EventCard key={event.id} title={event.title} location={event.location} time={event.time} cost={event.cost} userID={event.userID} />;
 			})}
 		</div>
 	);
