c69e723e6d286707da3e34b3c39b376fbc59a321
calbertan <95054192+calbertan@users.noreply.github.com>
2022-10-15 14:46:07 -0700

    updated eventForm to use bootstrap and react state

--- client/src/pages/EventForm.jsx
+++ client/src/pages/EventForm.jsx
@@ -1,33 +1,72 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 
+
 const EventForm = ()=>{
+	const[ticket,setTicket] = React.useState('');
+	const[location,setLocation] = React.useState('');
+	const[dateTime,setDateTime] = React.useState('');
+	const[cost,setCost] = React.useState('');
 	return (
-		<body>
-			<div class='EventForm'>
-				<h1>Add Event</h1>
-				<div class='eventForm-group'>
-					<label for='ticketName'>Ticket Name: </label>
-					<input type='text' id='ticketName'></input>
-				</div>
-				<div class='eventForm-group'>
-					<label for='Location'>Location: </label>
-					<input type='text' id='location'></input>
-				</div>
-				<div class='eventForm-group'>
-					<label for='timeDate'>Time/Date : </label>
-					<input type='text' id='timeDate'></input>
-				</div>
-				<div class='eventForm-group'>
-					<label for='originalCost'>Original Cost: </label>
-					<input type='text' id='originalCost'></input>
-				</div>
-				<div class='eventForm-group'>
-					<label for='contact'>Prefered way to contact: </label>
-					<input type='text' id='contact' placeholder='@example'></input>
-				</div>
-				<button type='submit' value='POST'></button>
-			</div>
-		</body>
+		<>
+			<Form>
+				<Form.Group className='mb-3' controlId='formTicketName'>
+					<Form.Label>Ticket Name</Form.Label>
+					<Form.Control 
+						type='ticket' 
+						placeholder='Name of the ticket you want to sell'
+						value={ticket}
+						onChange={({target})=>{
+							setTicket(target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formLocation'>
+					<Form.Label>Location</Form.Label>
+					<Form.Control 
+						type='location' 
+						placeholder='Location' 
+						value={location}
+						onChange={({target})=>{
+							setLocation(target.value);
+						}}/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formDateTime'>
+					<Form.Label>Date Time</Form.Label>
+					<Form.Control 
+						type='dateTime' 
+						placeholder='ex. Sunday, 16 October - 7.00pm' 
+						value={dateTime}
+						onChange={({target})=>{
+							setDateTime(target.value);
+						}}/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formCost'>
+					<Form.Label>Cost</Form.Label>
+					<Form.Control 
+						type='cost' 
+						placeholder='Cost of the ticket' 
+						value={cost}
+						onChange={({target})=>{
+							setCost(target.value);
+						}}/>
+				</Form.Group>
+
+				<Button 
+					variant='primary' 
+					onClick={()=>{
+						console.log(ticket);
+					}}>
+					Submit
+				</Button>
+			</Form>
+		</>
 	);
 };
 
