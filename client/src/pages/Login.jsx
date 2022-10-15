54e55a6ce34be64d231d7ae9f964765e30e74c42
Natalya Lebedeva <natashaswan.ca@gmail.com>
2022-10-15 14:11:17 -0700

    upd login, add states to manage input data

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,27 +1,56 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import {useState} from 'react';
 
 const Login = ()=>{
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
 	return (
 		<>
 			<Form>
 				<Form.Group className='mb-3' controlId='formBasicEmail'>
 					<Form.Label>Email address</Form.Label>
-					<Form.Control type='email' placeholder='Enter email' />
+					<Form.Control
+						type='email'
+						placeholder='Enter email'
+						value={email}
+						onChange={({target})=>{
+							setEmail(target.value);
+						}}
+					/>
 					<Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text>
 				</Form.Group>
 
 				<Form.Group className='mb-3' controlId='formBasicPassword'>
 					<Form.Label>Password</Form.Label>
-					<Form.Control type='password' placeholder='Password' />
+					<Form.Control
+						type='password'
+						placeholder='Password'
+						value={password}
+						onChange={({target})=>{
+							setPassword(target.value);
+						}}
+					/>
 				</Form.Group>
 				<Form.Group className='mb-3' controlId='formBasicCheckbox'>
 					<Form.Check type='checkbox' label='Check me out' />
 				</Form.Group>
-				<Button variant='primary' type='submit'>
-					Submit
+				<Button
+					variant='primary'
+					onClick={()=>{
+						console.log('sending data');
+					}}>
+					Sign In
+				</Button>
+				<Button
+					variant='primary'
+					onClick={()=>{
+						console.log('sending data');
+					}}>
+					Sign Up
 				</Button>
 			</Form>
 		</>
