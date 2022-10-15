6cbcb82cf42d4b5ff2a24febf35645b72fda3d94
Natalya Lebedeva <natashaswan.ca@gmail.com>
2022-10-15 12:34:52 -0700

    add routes to app and upd comps links to menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Login from "./components/Login";
 import Menu from './components/Menu';
+import EventForm from './components/EventForm';
+import Home from './components/Home';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
+    <div className="App">\<Router>
       <Menu />
+      <Routes>
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/' component={Home} />
+        <Route path='/add-event' component={EventForm} />
+      </Routes>
+    </Router>
     </div>
   );
 }
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -11,15 +11,21 @@ const Menu = ()=>{
 		<>
 			<Navbar bg='dark' variant='dark'>
 				<Container>
-					<Navbar.Brand href='#home'>
+					<Navbar.Brand as={Link} to='/'>
 						<img alt='' src='' width='30' height='30' className='d-inline-block align-top' /> Last Menute Tickets
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='me-auto'>
-							<Nav.Link as={Link}>Events</Nav.Link>
-							<Nav.Link as={Link}>Add Event</Nav.Link>
-							<Nav.Link as={Link}>Login</Nav.Link>
+							<Nav.Link as={Link} to='/'>
+								Events
+							</Nav.Link>
+							<Nav.Link as={Link} to='/add-event'>
+								Add Event
+							</Nav.Link>
+							<Nav.Link as={Link} to='/login'>
+								Login
+							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
