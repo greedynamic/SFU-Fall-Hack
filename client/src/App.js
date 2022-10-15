edd6807e2ec4fb9582c707b0c9cd97b384430dc9
Natalya Lebedeva <natashaswan.ca@gmail.com>
2022-10-15 12:01:35 -0700

    add menu component and svgs

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login";
-
-import Menu from './components/Menu';
-
 import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      <Login/>
-=======
-      <Menu/>
->>>>>>> Stashed changes
-=======
-      <Menu/>
->>>>>>> Stashed changes
+      <Login />
+      <Menu />
     </div>
   );
 }
