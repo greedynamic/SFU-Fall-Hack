2a01fc1b08ca3bf52a2ef116094fc0a210f62cbc
Natalya Lebedeva <natashaswan.ca@gmail.com>
2022-10-15 18:47:38 -0700

    upd home comp

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,39 +1,31 @@
-import EventCard from "../components/EventCard";
-import axios from "axios";
-import { useState, useContext, useEffect } from "react";
-import { Context } from "../context/GlobalContext";
+import EventCard from '../components/EventCard';
+import axios from 'axios';
+import {useState, useContext, useEffect} from 'react';
+import {Context} from '../context/GlobalContext';
 
-function Home() {
-  const userId = useContext(Context);
-  const [eventsData, setEventsData] = useState();
-  useEffect(() => {
-    console.log(userId);
+const Home = ()=>{
+	const userId = useContext(Context);
+	const [eventsData, setEventsData] = useState();
+	useEffect(()=>{
+		console.log(userId);
 
-    console.log("GETTING EVENTS");
+		console.log('GETTING EVENTS');
 
-    axios.get("http://localhost:5001/api/v1/tickets").then((response) => {
-      const data = response.data.tickets;
-      setEventsData(data);
-    });
-  }, []);
-  return (
-    <div>
-      {eventsData &&
-        eventsData.map((event) => {
-          console.log(event);
-          return (
-            <EventCard
-              key={event.tickets_id}
-              title={event.name}
-              location={event.location}
-              time={event.date}
-              cost={event.cost}
-              userID={event.user_id}
-              contact={event.contact}
-            />
-          );
-        })}
-    </div>
-  );
-}
+		axios.get('http://localhost:5001/api/v1/tickets').then((response)=>{
+			const data = response.data.tickets;
+			setEventsData(data);
+		});
+	}, []);
+	return (
+		<div>
+			{eventsData &&
+				eventsData.map((event)=>{
+					console.log(event);
+					return (
+						<EventCard key={event.tickets_id} title={event.name} location={event.location} time={event.date} cost={event.cost} userID={event.user_id} contact={event.contact} />
+					);
+				})}
+		</div>
+	);
+};
 export default Home;
